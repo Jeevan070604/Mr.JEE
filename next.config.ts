5557abb849bc6f04fe14de7cf5982be271360ba3
RESTARNT Owner <restarnt@example.com>
2026-03-17 19:51:20 +0530

    Update config for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: 'export',
   distDir: 'dist',
+  basePath: '/Mr.JEE',
+  assetPrefix: '/Mr.JEE',
   images: {
     unoptimized: true,
   },
